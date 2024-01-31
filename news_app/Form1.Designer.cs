74476e80f755ba9d47b099ea130ccac30b79b347
朝安 謝 <andyssvs015@gmail.com>
2024-01-31 10:10:09 +0800

    加入專案檔案。

---
+++ news_app/Form1.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace news_app
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.flowLayout_news = new System.Windows.Forms.FlowLayoutPanel();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.checkBox6 = new System.Windows.Forms.CheckBox();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.nxt_page = new System.Windows.Forms.Button();
+            this.last_page = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // flowLayout_news
+            // 
+            this.flowLayout_news.AutoScroll = true;
+            this.flowLayout_news.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.flowLayout_news.Location = new System.Drawing.Point(12, 12);
+            this.flowLayout_news.Name = "flowLayout_news";
+            this.flowLayout_news.Size = new System.Drawing.Size(500, 389);
+            this.flowLayout_news.TabIndex = 0;
+            this.flowLayout_news.WrapContents = false;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Font = new System.Drawing.Font("微軟正黑體", 14.25F);
+            this.dateTimePicker1.Location = new System.Drawing.Point(6, 76);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(258, 33);
+            this.dateTimePicker1.TabIndex = 1;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.checkBox6);
+            this.groupBox1.Controls.Add(this.checkBox5);
+            this.groupBox1.Controls.Add(this.checkBox4);
+            this.groupBox1.Controls.Add(this.checkBox3);
+            this.groupBox1.Controls.Add(this.checkBox2);
+            this.groupBox1.Controls.Add(this.checkBox1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.dateTimePicker1);
+            this.groupBox1.Font = new System.Drawing.Font("微軟正黑體", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.groupBox1.Location = new System.Drawing.Point(518, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(270, 293);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "搜索";
+            // 
+            // checkBox6
+            // 
+            this.checkBox6.AutoSize = true;
+            this.checkBox6.Font = new System.Drawing.Font("微軟正黑體", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.checkBox6.Location = new System.Drawing.Point(10, 256);
+            this.checkBox6.Name = "checkBox6";
+            this.checkBox6.Size = new System.Drawing.Size(66, 21);
+            this.checkBox6.TabIndex = 9;
+            this.checkBox6.Text = "中央社";
+            this.checkBox6.UseVisualStyleBackColor = true;
+            // 
+            // checkBox5
+            // 
+            this.checkBox5.AutoSize = true;
+            this.checkBox5.Font = new System.Drawing.Font("微軟正黑體", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.checkBox5.Location = new System.Drawing.Point(10, 228);
+            this.checkBox5.Name = "checkBox5";
+            this.checkBox5.Size = new System.Drawing.Size(66, 21);
+            this.checkBox5.TabIndex = 8;
+            this.checkBox5.Text = "聯合報";
+            this.checkBox5.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Font = new System.Drawing.Font("微軟正黑體", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.checkBox4.Location = new System.Drawing.Point(10, 200);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(79, 21);
+            this.checkBox4.TabIndex = 7;
+            this.checkBox4.Text = "經濟日報";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Font = new System.Drawing.Font("微軟正黑體", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.checkBox3.Location = new System.Drawing.Point(10, 172);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(79, 21);
+            this.checkBox3.TabIndex = 6;
+            this.checkBox3.Text = "工商時報";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Font = new System.Drawing.Font("微軟正黑體", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.checkBox2.Location = new System.Drawing.Point(10, 144);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(79, 21);
+            this.checkBox2.TabIndex = 5;
+            this.checkBox2.Text = "自由時報";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("微軟正黑體", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.checkBox1.Location = new System.Drawing.Point(10, 116);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(79, 21);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "中國時報";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.label1.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(6, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 24);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "關鍵字:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(83, 31);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(181, 39);
+            this.textBox1.TabIndex = 2;
+            // 
+            // nxt_page
+            // 
+            this.nxt_page.Font = new System.Drawing.Font("微軟正黑體", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.nxt_page.Location = new System.Drawing.Point(437, 407);
+            this.nxt_page.Name = "nxt_page";
+            this.nxt_page.Size = new System.Drawing.Size(75, 23);
+            this.nxt_page.TabIndex = 3;
+            this.nxt_page.Text = "下一頁";
+            this.nxt_page.UseVisualStyleBackColor = true;
+            // 
+            // last_page
+            // 
+            this.last_page.Font = new System.Drawing.Font("微軟正黑體", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.last_page.Location = new System.Drawing.Point(12, 407);
+            this.last_page.Name = "last_page";
+            this.last_page.Size = new System.Drawing.Size(75, 23);
+            this.last_page.TabIndex = 4;
+            this.last_page.Text = "上一頁";
+            this.last_page.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.last_page);
+            this.Controls.Add(this.nxt_page);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.flowLayout_news);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "新聞入口";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayout_news;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.CheckBox checkBox6;
+        private System.Windows.Forms.CheckBox checkBox5;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button nxt_page;
+        private System.Windows.Forms.Button last_page;
+    }
+}
+
---
+++ news_app/Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Windows.Forms;
+using MongoDB.Driver;
+using MongoDB.Bson;
+using System.Drawing;
+using System.Configuration;
+using System.Collections.Specialized;
+
+namespace news_app
+{
+    public partial class Form1 : Form
+    {
+        private void title_Click(object sender, EventArgs e)
+        {
+            Label Lbl = (Label)sender;
+            System.Diagnostics.Process.Start(Lbl.Name);
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            string acc = ConfigurationManager.AppSettings.Get("account");
+            string pwd = ConfigurationManager.AppSettings.Get("password");
+
+            var dbClient = new MongoClient("mongodb+srv://"+acc+":"+pwd+"@tw-news.mbrlkzb.mongodb.net/");
+            var database = dbClient.GetDatabase("tw-news");
+
+            string[] news_col = { "CHT", "LTN", "UDN", "CNA", "CTEE", "Money" };
+
+            foreach (var col_name in news_col)
+            {
+                var collection = database.GetCollection<BsonDocument>(col_name);
+                var Documents = collection.Find(new BsonDocument()).ToList();
+                foreach (var Document in Documents)
+                {
+
+                    PictureBox Pctbox_news = new PictureBox();
+                    Label Lbl_news_title = new Label();
+                    Label Lbl_news_content = new Label();
+                    FlowLayoutPanel Flp_title_content = new FlowLayoutPanel();
+                    Flp_title_content.FlowDirection = FlowDirection.TopDown;
+                    Flp_title_content.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+
+                    Panel outerPanel = new Panel();
+                    outerPanel.Size = new Size(flowLayout_news.Size.Width - 30, 200);
+                    //outerPanel.AutoSize = true;
+                    outerPanel.BorderStyle = BorderStyle.FixedSingle;
+                    outerPanel.BackColor = Color.FromArgb(240, 248, 255);
+
+                    TableLayoutPanel tableLayoutPanel = new TableLayoutPanel();
+                    tableLayoutPanel.Size = new Size(outerPanel.Size.Width, 200);
+                    tableLayoutPanel.RowCount = 1;
+                    tableLayoutPanel.ColumnCount = 2;
+
+                    //圖片
+                    Pctbox_news = new PictureBox();
+                    if (Document.Contains("img"))
+                    {
+                        if (Document.GetElement("img").Value.ToString() != "")
+                        {
+                            try
+                            {
+                                Pctbox_news.Load(Document.GetElement("img").Value.ToString());
+                                Pctbox_news.Text = Document.GetElement("title").Value.ToString();
+                                Pctbox_news.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+                                Pctbox_news.SizeMode = PictureBoxSizeMode.StretchImage;
+                            }
+                            catch (Exception e)
+                            {
+                                Console.WriteLine(e.Message);
+                            }
+
+                        }
+                        else
+                        {
+                            Pctbox_news.Load("../../../no-pictures.png");
+                            Pctbox_news.Text = Document.GetElement("title").Value.ToString();
+                            Pctbox_news.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+                            Pctbox_news.SizeMode = PictureBoxSizeMode.StretchImage;
+
+                        }
+                    }
+                    else
+                    {
+                        Pctbox_news.Load("../../../no-pictures.png");
+                        Pctbox_news.Text = Document.GetElement("title").Value.ToString();
+                        Pctbox_news.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+                        Pctbox_news.SizeMode = PictureBoxSizeMode.StretchImage;
+                    }
+
+
+                    //標題
+                    Lbl_news_title = new Label();
+                    Lbl_news_title.Text = Document.GetElement("title").Value.ToString();
+                    Lbl_news_title.AutoSize = true;
+                    Lbl_news_title.Name = Document.GetElement("link").Value.ToString();
+                    Lbl_news_title.Font = new Font("微軟正黑體", 12, FontStyle.Bold);
+
+                    Lbl_news_title.Click += title_Click;
+
+
+
+                    //內容
+                    if (Document.Contains("content"))
+                    {
+                        Lbl_news_content = new Label();
+                        Lbl_news_content.Text = "\n"+Document.GetElement("content").Value.ToString();
+                        Lbl_news_content.Font = new Font("微軟正黑體", 8, FontStyle.Bold);
+                        Lbl_news_content.AutoSize = true;
+
+                    }
+                    else
+                    {
+                        Lbl_news_content = new Label();
+                        Lbl_news_content.Text = "";
+                        Lbl_news_content.AutoSize = true;
+                    }
+
+
+                    Flp_title_content.Controls.Add(Lbl_news_title);
+                    Flp_title_content.Controls.Add(Lbl_news_content);
+
+
+                    // 建立左控制項
+                    tableLayoutPanel.Controls.Add(Pctbox_news, 0, 0);
+
+                    // 建立右控制項
+                    tableLayoutPanel.Controls.Add(Flp_title_content, 1, 0);
+                    
+
+                    // 設定列的大小比例
+                    tableLayoutPanel.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50));  // 左列佔25%
+                    tableLayoutPanel.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50));  // 右列佔75%
+
+
+                    outerPanel.Controls.Add(tableLayoutPanel);
+
+                    //Flp_g_pctlbl.Controls.Add(Pctbox_news);
+                    //Flp_g_pctlbl.Controls.Add(Lbl_news_title);
+                    //Flp_g_pctlbl.Controls.Add(Lbl_news_content);
+
+                    flowLayout_news.Controls.Add(outerPanel);
+
+                }
+            }
+
+
+            /*var filter = Builders<BsonDocument>.Filter.Gte("your_date_field", startDate) &
+                    Builders<BsonDocument>.Filter.Lt("your_date_field", endDate);
+            var Documents = collection.Find(filter).ToList();*/
+        }
+    }
+}
